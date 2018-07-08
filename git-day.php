0a5d1ff93f28cae6bd5deaa742884feb67180ac2
kisa002 <vnycall74@naver.com>
2018-07-08 18:42:07 +0900

    화면 가운데 정렬

--- git-day.php
+++ git-day.php
@@ -62,52 +62,66 @@
             {
                 background-color: #25AAFF;
                 color: #FFFFFF;
+
+                display: table;
+                margin: 0 auto;
+
+                height: 100%;
+            }
+            
+            .container
+            {
+                height: 100%;
+                display: table-cell;
+                vertical-align: middle;
             }
         </style>
     </head>
 
     <body>
-        <div>당신의 1일 1커밋은 <b><?php echo $cont ?>일</b> 간 지속되었습니다!</div>
+        <div class="container">
+            <div>당신의 1일 1커밋은 <b><?php echo $cont ?>일</b> 간 지속되었습니다!</div>
 
-        <div>하지만, 아쉽게도 <b><?php echo $miss ?>일</b>을 커밋하는데 실패하셨습니다 ㅠㅠ</div>
-        <br>
-        <div>365일 커밋까지 <b><?php echo (365-$cont) ?>일</b> 남았습니다!</div>
-        <br>
-        <div>아름다운 잔디밭까지는 <b><?php echo($max - $cont - 1)?>일</b> 남았습니다:)</div>
-        <br>
-        <div><?php echo $today ?></div>
-        <br>
+            <div>하지만, 아쉽게도 <b><?php echo $miss ?>일</b>을 커밋하는데 실패하셨습니다 ㅠㅠ</div>
+            <br>
+            <div>365일 커밋까지 <b><?php echo (365-$cont) ?>일</b> 남았습니다!</div>
+            <br>
+            <div>아름다운 잔디밭까지는 <b><?php echo($max - $cont - 1)?>일</b> 남았습니다:)</div>
+            <br>
+            <div><?php echo $today ?></div>
+            <br>
 
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <div id="chart_div"></div>
-        
-        <script>
-            google.charts.load('current', {packages: ['corechart', 'line']});
-            google.charts.setOnLoadCallback(drawBasic);
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <div id="chart_div"></div>
+            
+            <script>
+                google.charts.load('current', {packages: ['corechart', 'line']});
+                google.charts.setOnLoadCallback(drawBasic);
 
-            function drawBasic() {
+                function drawBasic() {
 
-                var data = new google.visualization.DataTable();
-                data.addColumn('number', 'X');
-                data.addColumn('number', 'Commit Count');
+                    var data = new google.visualization.DataTable();
+                    data.addColumn('number', 'X');
+                    data.addColumn('number', 'Commit Count');
 
-                data.addRows([<?php echo $row ?>]);
+                    data.addRows([<?php echo $row ?>]);
 
-                var options = {
-                    hAxis: {
-                    title: 'Date'
-                    },
-                    vAxis: {
-                    title: 'Commit'
-                    }
-                };
+                    var options = {
+                        hAxis: {
+                        title: 'Date'
+                        },
+                        vAxis: {
+                        title: 'Commit'
+                        }
+                    };
 
-                var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
-                chart.draw(data, options);
-            }
+                    chart.draw(data, options);
+                }
 
-            drawBasic();
-        </script>
+                drawBasic();
+            </script>
+        </div>
     </body>
 </html>
